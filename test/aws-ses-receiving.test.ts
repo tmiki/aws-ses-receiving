053b24c0de32cf66d3e081c268ecfad06ca01ab5
tmiki <sidhemail@gmail.com>
2025-03-22 13:12:28 +0900

    Initial commit

---
+++ test/aws-ses-receiving.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsSesReceiving from '../lib/aws-ses-receiving-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-ses-receiving-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsSesReceiving.AwsSesReceivingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
